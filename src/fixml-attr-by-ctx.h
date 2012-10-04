f17d87538d9af4b97a0db25aa5aa0507c3332275
Sebastian Freundt <freundt@ga-group.nl>
2012-10-04 09:44:34 +0000

    fix, include necessary headers in fixml-attr-by-ctx.h and name the msg slot type correctly
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/fixml-attr-by-ctx.h
+++ src/fixml-attr-by-ctx.h
@@ -1,12 +1,18 @@
 #if !defined INCLUDED_fixml_attr_by_ctx_h_
 #define INCLUDED_fixml_attr_by_ctx_h_
 
+#include <stdint.h>
+#include <stddef.h>
+#include "fixml-attr.h"
+#include "fixml-comp.h"
+#include "fix-msg-type.h"
+
 extern fixc_attr_t fixc_get_aid(
 	union __attribute__((transparent_union)) {
 		unsigned int i;
 		uint16_t ui16;
 		fixc_comp_t comp;
-		fixc_msg_t msg;
+		fixc_msg_type_t msg;
 	}, const char *attr, size_t alen);
 
 #endif	/* INCLUDED_fixml_attr_by_ctx_h_ */
