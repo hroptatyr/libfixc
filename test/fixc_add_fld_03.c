03c884da0ac7e509a98e3a494b2038c3dd643de2
Sebastian Freundt <freundt@ga-group.nl>
2012-10-22 16:43:22 +0000

    test, provide unit tests for fixml_add_fld_at()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/fixc_add_fld_03.c
@@ -0,0 +1,48 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "fix.h"
+#include "fixml-msg.h"
+#include "fixml-attr.h"
+
+#define countof(x)	(sizeof(x) / sizeof(*x))
+
+int
+main(void)
+{
+/* starting out with an empty message, add 2 Quot's */
+	static const struct fixc_fld_s f_quot = {
+		.tag = FIXC_MSG_TYPE,
+		.typ = FIXC_TYP_MSGTYP,
+		.mtyp = (fixc_msgt_t)FIXML_MSG_Quote,
+	};
+	fixc_msg_t msg = make_fixc_msg((fixc_msgt_t)FIXC_MSGT_UNK);
+	int res = 0;
+
+	/* now adding a tag 35 should result in occupying slot f35 */
+	fixc_add_fld_at(msg, f_quot, 0);
+
+	if (msg->nflds != 1) {
+		fprintf(stderr, "expected 1 field, got %zu\n", msg->nflds);
+		res = 1;
+	}
+
+	if (msg->f35.mtyp != FIXC_MSGT_BATCH) {
+		fprintf(stderr, "expected static f35 to be Batch, got %u\n",
+			(unsigned int)msg->f35.mtyp);
+		res = 1;
+	}
+
+	if (msg->flds[0].tag != FIXC_MSG_TYPE ||
+	    msg->flds[0].mtyp != FIXML_MSG_Quote) {
+		fprintf(stderr, "expected fld[0] to be Quot, got %hu (%u)\n",
+			msg->flds[0].tag, (unsigned int)msg->flds[0].mtyp);
+		res = 1;
+	}
+
+	free_fixc(msg);
+	return res;
+}
+
+/* fixc_add_fld_03.c ends here */
