89f011a9ad9042938d019d6b4e56bdba09df1318
Sebastian Freundt <freundt@ga-group.nl>
2012-10-31 17:01:00 +0000

    minor, in fixc_fixup() comb .tpc = .cnt = 0 fields in a second cycle when run out of stack
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/fixml-wr.c
+++ src/fixml-wr.c
@@ -554,6 +554,7 @@ fixc_fixup(fixc_msg_t msg)
 	} stk[16];
 	ssize_t nstk = -1;
 	unsigned int streak = 0;
+	int out_of_stack_p = 0;
 
 #define rset()		(streak = 0)
 #define push(x, i)	(rset(),				\
@@ -563,6 +564,7 @@ fixc_fixup(fixc_msg_t msg)
 #define peeki()		stk[nstk].idx
 #define peek()		stk[nstk].ctx
 #define pop()		(streak = 0, stk[nstk--].ctx)
+#define streak()	(streak++)
 
 	push(msg->f35.mtyp, 0);
 	for (size_t i = 0; i < msg->nflds; i++) {
@@ -596,6 +598,7 @@ fixc_fixup(fixc_msg_t msg)
 
 		FIXC_DEBUG("couldn't find context for %hu\n", msg->flds[i].tag);
 		push(msg->f35.mtyp, 0);
+		out_of_stack_p = 1;
 		continue;
 	succ:
 		msg->flds[i].tpc = (uint16_t)peek().ui16;
@@ -603,7 +606,24 @@ fixc_fixup(fixc_msg_t msg)
 		    msg->flds[peeki() + 1].tag == msg->flds[i].tag) {
 			rset();
 		}
-		msg->flds[i].cnt = (uint16_t)streak++;
+		msg->flds[i].cnt = (uint16_t)streak();
+	}
+
+	/* plain operation, but only if we encountered out_of_stack */
+	if (UNLIKELY(out_of_stack_p)) {
+		fixc_ctxt_t last = {FIXC_MSGT_UNK};
+
+		streak = 0;
+		for (size_t i = 0; i < msg->nflds; i++) {
+			streak++;
+			if (!msg->flds[i].tpc && !msg->flds[i].cnt) {
+				msg->flds[i].tpc = (uint16_t)last.ui16;
+				msg->flds[i].cnt = (uint16_t)streak;
+			} else if (!msg->flds[i].cnt) {
+				streak = 0;
+				last.ui16 = msg->flds[i].tpc;
+			}
+		}
 	}
 	return;
 }
