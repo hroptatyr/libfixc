9cfbd78b3bdff55b4ce51398354a391ffc51f904
Sebastian Freundt <freundt@ga-group.nl>
2013-03-20 13:49:52 +0000

    test, add regressions for Batch only empty message rendering
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

---
+++ test/fixc_to_fixml_01.c
@@ -0,0 +1,37 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "fix.h"
+
+int
+main(void)
+{
+	static char res[] = "\
+<FIXML xmlns=\"http://www.fixprotocol.org/FIXML-5-0-SP2\" v=\"5.0 SP2\">\
+<Batch></Batch>\
+</FIXML>\
+";
+	fixc_msg_t msg;
+	struct fixc_rndr_s r;
+
+	/* start a fix msg for that */
+	msg = make_fixc_msg((fixc_msgt_t)FIXC_MSGT_BATCH);
+
+	/* render the whole shebang */
+	r = fixc_render_fixml_rndr(msg);
+	free_fixc(msg);
+
+	if (r.len != sizeof(res) - 1) {
+		fprintf(stderr, "sizes differ %zu v %zu\n%s\n  v\n%s\n",
+			r.len, sizeof(res) - 1,
+			r.str, res);
+		return 1;
+	} else if (memcmp(r.str, res, r.len)) {
+		fprintf(stderr, "buffers differ:\n%s\n  v\n%s\n", r.str, res);
+		return 1;
+	}
+	return 0;
+}
+
+/* fixc_to_fixml_01.c ends here */
---
+++ test/fixc_to_fixml_03.c
@@ -0,0 +1,38 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "fix.h"
+
+int
+main(void)
+{
+	static char res[] = "\
+<FIXML xmlns=\"http://www.fixprotocol.org/FIXML-5-0-SP2\" v=\"5.0 SP2\">\
+<Batch></Batch>\
+</FIXML>\
+";
+	static char buf[4096U];
+	fixc_msg_t msg;
+	size_t z;
+
+	/* start a fix msg for that */
+	msg = make_fixc_msg((fixc_msgt_t)FIXC_MSGT_BATCH);
+
+	/* render the whole shebang */
+	z = fixc_render_fixml(buf, sizeof(buf), msg);
+	free_fixc(msg);
+
+	if (z != sizeof(res) - 1) {
+		fprintf(stderr, "sizes differ %zu v %zu\n%s\n  v\n%s\n",
+			z, sizeof(res) - 1,
+			buf, res);
+		return 1;
+	} else if (memcmp(buf, res, z)) {
+		fprintf(stderr, "buffers differ:\n%s\n  v\n%s\n", buf, res);
+		return 1;
+	}
+	return 0;
+}
+
+/* fixc_to_fixml_03.c ends here */
